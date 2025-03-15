64db583c511802f0868a1d44346803905fb89b9f
SKYBIRDSGP <sgpatki3422@gmail.com>
2025-03-15 12:19:14 +0530

    Creating our own QL env

--- QL/scripts/ql_env.py
+++ QL/scripts/ql_env.py
@@ -19,3 +19,68 @@ SHOW_EVERY = 3000
 
 start_q_table = None # or file name
 
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+
+PLAYER_N = 1
+FOOD_N = 2
+ENEMY_N = 3
+# Color of the Blobs 
+d = {1:(255,175,0),
+     2:(0, 255, 0),
+     3:(0, 0, 255)}
+
+## Making the blob class
+class blob:
+    def __init__(self):
+        self.x = np.random.randint(0,SIZE)
+        self.y = np.random.randint(0,SIZE)
+    def __str__(self):
+        return f"{self.x}, {self.y}"
+    
+    def __sub__(self,other):
+        return(self.x - other.x , self.y - other.y)
+    
+    def action(self,choice):
+        if choice == 0:
+            self.move(x=1, y=1) 
+        elif choice == 1:
+            self.move(x=-1, y=-1)
+        elif choice == 2:
+            self.move(x=-1, y=1)
+        elif choice == 3:
+            self.move(x=1, y=-1)    ## Player moves diagonlly only
+
+    def move(self, x=False, y=False):
+        if not x:
+            self.x += np.random.randint(-1,2)
+        else:
+            self.x += x
+
+        if not y:
+            self.y += np.random.randint(-1,2)
+        else:
+            self.y += y
+        
+        if self.x < 0:
+            self.x = 0
+        elif self.x > SIZE-1:
+            self.x = SIZE-1
+
+        if self.y < 0:
+            self.y = 0
+        elif self.y > SIZE-1:
+            self.y = SIZE-1
+        
+if start_q_table is None:
+    q_table = {}
+    for x1 in range(-SIZE+1, SIZE):
+        for y1 in range(-SIZE+1, SIZE):
+            for x2 in range(-SIZE+1, SIZE):
+                for y2 in range(-SIZE+1, SIZE):
+                    q_table[((x1,y1),(x2,y2))] = []
+
+
+        
+
+
