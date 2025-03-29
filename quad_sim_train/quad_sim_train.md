3333ac2a40544b98e8dc1c8400ca2c9e5457a63a
SKYBIRDSGP <sgpatki3422@gmail.com>
2025-03-29 15:30:36 +0530

    Readme Updates

--- quad_sim_train/quad_sim_train.md
+++ quad_sim_train/quad_sim_train.md
@@ -7,3 +7,18 @@ Now, after learning some of the basics of RL, let us proceed with how to use RL 
 These are the very two basic cases we will implement initially, and then add more conditions for example maintaining the Torso at some height, some acceleration, gaiting, etc.
 
 For instance, we will be using the A1 Quadruped.
+
+``` python
+loadURDF(urdf_path, orientation, quaterion, urdf_flag, fixed_base) : 
+
+# This loads the URDF file in the PyBullet , with the path to the URDF file, the orientation, collision flag and whether the base is fixed(True or False)
+
+getNumJoints(asset) : # Returns the total number of joints in the robot
+
+getJointInfo(asset, joint_id)[2] : # Returns the joint type
+
+getJointInfo(asset, joint_id)[8] : # Returns the lower limit of the joint angle
+
+getJointInfo(asset, joint_id)[9] : # Returns the upper limit of the joint angle
+
+```
--- quad_sim_train/scripts/quad_sim.py
+++ quad_sim_train/scripts/quad_sim.py
@@ -14,9 +14,20 @@ p.setTimeStep(1./500)
 urdfFlags = p.URDF_USE_SELF_COLLISION
 urdf_path = os.path.join(os.path.dirname(__file__), "../quad_description/urdf/a1.urdf")
 
-robot = p.loadURDF(urdf_path, [0,0,0.48],[0,0,0,1], flags = urdfFlags, useFixedBase = False)
+robot = p.loadURDF(urdf_path, [0,0,0.48],[0,0,0,1], flags = urdfFlags, useFixedBase = True)
 print(urdf_path)
+print("__________________")
+num_joints = p.getNumJoints(robot)
+print(f"Number of joints: {num_joints}")
 
+for j in range(num_joints):
+    joint_info = p.getJointInfo(robot, j)  # Get joint details
+    joint_id = joint_info[0]  # Joint index
+    joint_name = joint_info[1].decode("utf-8")  # Joint name (needs decoding)
+    link_name = joint_info[12].decode("utf-8")  # Link name (needs decoding)
+    link = p.getLinkState(robot, j)
+    print(f"Joint ID: {joint_id}, Joint Name: {joint_name}, Link Name: {link_name}\n")
+    print(link)
 while True: 
     p.stepSimulation()
     time.sleep(1./500)
